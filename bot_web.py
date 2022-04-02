0a68572e8f80c9ce2114469cd195231280fa85e9
Lucas Azevedo <57512201+lucasazevedo01@users.noreply.github.com>
2022-04-02 09:09:56 -0300

    Update bot_web.py

--- bot_web.py
+++ bot_web.py
@@ -9,17 +9,15 @@ today = datetime.today()
 today2 = today.strftime("%d/%m/%Y")
 
 #Variavel com a descrição da nota fiscal
-descricao_nota = 'REFERENTE A MANUTENÇÃO E REPARO DE OUTRAS MÁQUINAS E EQUIPAMENTOS PARA USOS'\
-    ' INDUSTRIAIS. Em conformidade com a LEI Nº 12.741, o valor aproximado dos tributos incidentes'\
-    ' nesta nota é de R$ 1250,00.'
+descricao_nota = 'Descrição dos serviços'
 
 
 navegador = webdriver.Chrome()
 navegador.get("https://nfse.vitoria.es.gov.br/")
 
 #Fazer login
-navegador.find_element_by_xpath('//*[@id="login"]').send_keys("aluizio-azevedo1@hotmail.com")
-navegador.find_element_by_xpath('//*[@id="senha"]').send_keys("LIMA2010")
+navegador.find_element_by_xpath('//*[@id="login"]').send_keys("seuemail@email.com")
+navegador.find_element_by_xpath('//*[@id="senha"]').send_keys("suasenha")
 navegador.find_element_by_xpath('//*[@id="formEntrada"]/div[2]/span/span/input').click()
 
 #Entrar na empresa
@@ -35,10 +33,10 @@ navegador.find_element_by_xpath('//*[@id="btNova"]/a/img').click()
 
 #Preenchimento NF
 navegador.find_element_by_xpath('//*[@id="formNovo"]/div[1]/div[2]/input[2]').click()
-navegador.find_element_by_xpath('//*[@id="formNovo"]/div[2]/div[2]/input').send_keys("04.999.675/0001-47")
-navegador.find_element_by_xpath('//*[@id="formNovo"]/div[3]/div[2]/input').send_keys("29160450")
-navegador.find_element_by_xpath('//*[@id="formNovo"]/div[4]/div[2]/input[1]').send_keys("506")
-navegador.find_element_by_xpath('//*[@id="formNovo"]/div[5]/div[2]/select').send_keys("BRASIL")
+navegador.find_element_by_xpath('//*[@id="formNovo"]/div[2]/div[2]/input').send_keys("CNJP_CLIENTE")
+navegador.find_element_by_xpath('//*[@id="formNovo"]/div[3]/div[2]/input').send_keys("CEP_CLIENTE")
+navegador.find_element_by_xpath('//*[@id="formNovo"]/div[4]/div[2]/input[1]').send_keys("NUM_CLIENTE")
+navegador.find_element_by_xpath('//*[@id="formNovo"]/div[5]/div[2]/select').send_keys("PAIS_CLIENTE")
 navegador.find_element_by_xpath('//*[@id="formNovo"]/div[2]/div[4]/input').click()
 
 #Preencher prestação de serviço linha 39 preenchimento da data
@@ -70,7 +68,7 @@ navegador.find_element_by_xpath('//*[@id="dvSelectAliquota"]/select/option[3]').
 navegador.find_element_by_xpath('//*[@id="formNovo"]/h4[6]').click()
 
 #Click para emitir a NF
-#navegador.find_element_by_xpath('//*[@id="btEncerrar"]/a/img').click()
+navegador.find_element_by_xpath('//*[@id="btEncerrar"]/a/img').click()
 
 #click no pop-up de confirmação
-#pyautogui.click(768, 244)
+pyautogui.click(768, 244)
